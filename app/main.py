4f5fa47027150054a376befb6d03ea9282991cf6
Ploycahnok-cp <168716196+Ploycahnok-cp@users.noreply.github.com>
2024-09-17 10:01:17 +0700

    update dynamic routes in main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-# from app.configs import Settings
+from fastapi.routing import APIRouter
+import importlib
+import os
 
 app = FastAPI()
 
 # CORS
-origins = [
-    "http://localhost:5173",
-    "https://main.dagez0h56fw8t.amplifyapp.com",
-    "https://staging.dagez0h56fw8t.amplifyapp.com",
-    "https://dev.dagez0h56fw8t.amplifyapp.com",
-    "https://rmi.cpgcro.com",
-]
+origins = ["*"]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -21,7 +17,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# dynamic routes
+route_files = [file for file in os.listdir("app/routes") if file.endswith(".py")]
+for file in route_files:
+    module_name = f"app.routes.{file[:-3]}"
+    module = importlib.import_module(module_name)
 
-@app.get("/api/v1/ping")
-async def check_connection():
-    return {"msg": "pong", "code": 200}
+    if hasattr(module, "router") and isinstance(module.router, APIRouter):
+        app.include_router(module.router)
